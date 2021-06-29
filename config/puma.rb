e2cd3c1ea06122462cf323c609ee4fb916ff06a0
baryshych <luka.baryshych@gmail.com>
2021-06-29 12:15:30 +0300

    initial

--- config/puma.rb
+++ config/puma.rb
@@ -3,7 +3,12 @@
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['SINATRA_MAX_THREADS'] || 5)
 threads threads_count, threads_count
+root = "#{Dir.getwd}"
 
 rackup      DefaultRackup
 port        ENV['PORT']     || 9292
 environment ENV['RACK_ENV'] || 'development'
+activate_control_app "tcp://127.0.0.1:9293"
+bind "unix://#{root}/tmp/puma.sock"
+pidfile "#{root}/tmp/pids/puma.pid"
+state_path "#{root}/tmp/pids/puma.state"
